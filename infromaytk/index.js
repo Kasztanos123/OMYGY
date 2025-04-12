8fd6ee08b9dc14d40f15ff9aa7ff1c9a3e1ac887
Thomasss-s <jabkoszing@gmail.com>
2025-04-12 20:48:44 +0200

    Add files via upload

---
+++ infromaytk/index.js
@@ -0,0 +1,13 @@
+function Login()
+{
+    let username =  document.getElementById("username-value").value;
+    let password = document.getElementById("password-value").value;
+    console.log(username + " " + password);
+
+
+    if (username.lenght && password.lenght > 5) 
+    {
+        window.location.href = "main.html";
+    }
+
+}
